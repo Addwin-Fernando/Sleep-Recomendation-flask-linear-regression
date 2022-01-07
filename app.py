af937e5ded4247e22f534d27ff0c9d3940c7144a
Addwin-Fernando <addwinf07@gmail.com>
2022-01-07 09:58:02 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template, request
+import model 
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def hello_world():
+    print(request.method)
+    if request.method == 'POST':
+        age = request.values.get("age")
+        sleep = model.predict(age)
+        sleep = round(sleep)
+
+
+        return render_template('index.html',age = age,sleep = sleep)
+        
+
+    return render_template('index.html')
+
+
+
+
+if __name__ =='__main__':
+    app.run(port=3000,debug=True)    
---
+++ model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np  
+import matplotlib.pyplot as plt  
+
+url = "Sleep prediction by age group\Sleep-age-updated - Sheet1.csv"
+s_data = pd.read_csv(url)
+
+s_data.head(10)
+
+X = s_data.iloc[:, :-1].values  
+y = s_data.iloc[:, 1].values
+
+from sklearn.model_selection import train_test_split  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from sklearn.linear_model import LinearRegression  
+regressor = LinearRegression()  
+regressor.fit(X_train, y_train)
+
+def predict(age):
+    own_pred = regressor.predict([[age]])
+    sleep = own_pred[0]
+    return sleep
+
+
